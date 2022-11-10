9e36b064f3057bfa36227c32372be79bb797af90
donald163duck <alexpodrolla@gmail.com>
2022-11-10 17:29:53 +0400

    Initial commit

---
+++ src/main/java/Program.java
@@ -0,0 +1,48 @@
+import java.util.Optional;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter the expression : ");
+
+        Optional<SimpleOperation> operation = parseOperation(reader);
+
+        if (operation.isPresent()) {
+            printResult(operation.get());
+        }
+
+        Number r = 8.21;
+
+    }
+
+    private static void printResult(SimpleOperation simpleOperation) {
+        try {
+            Number result = simpleOperation.getResult();
+            System.out.printf(simpleOperation + " = " + result);
+        } catch (Exception e) {
+            System.out.println("Unable to calculate result of this operation " + simpleOperation);
+        }
+    }
+
+    public static Optional<SimpleOperation> parseOperation(Scanner scanner) {
+        try {
+            Number num1 = readNextNumber(scanner);
+            char op = scanner.next().charAt(0);
+            Number num2 = readNextNumber(scanner);
+            return Optional.of(new SimpleOperation(num1, op, num2));
+        } catch (Exception e) {
+            System.out.printf("Unable to parse operation, cause: %s", e.getMessage());
+            return Optional.empty();
+        }
+    }
+
+    public static Number readNextNumber(Scanner scanner) {
+        Number num1;
+        if (scanner.hasNextInt()) {
+            return scanner.nextInt();
+        } else {
+            return scanner.nextFloat();
+        }
+    }
+}
---
+++ src/main/java/SimpleOperation.java
@@ -0,0 +1,62 @@
+import java.math.BigDecimal;
+
+public class SimpleOperation {
+    private Number num1;
+    private char op;
+    private Number num2;
+
+    public Number getNum1() {
+        return num1;
+    }
+
+    public void setNum1(Number num1) {
+        this.num1 = num1;
+    }
+
+    public char getOp() {
+        return op;
+    }
+
+    public void setOp(char op) {
+        this.op = op;
+    }
+
+    public Number getNum2() {
+        return num2;
+    }
+
+    public void setNum2(Number num2) {
+        this.num2 = num2;
+    }
+
+    public SimpleOperation() {
+    }
+
+    public SimpleOperation(Number num1, char op, Number num2) {
+        this.num1 = num1;
+        this.op = op;
+        this.num2 = num2;
+    }
+
+    public Number getResult() {
+        BigDecimal bigDecimal1 = new BigDecimal(num1.floatValue());
+        BigDecimal bigDecimal2 = new BigDecimal(num2.floatValue());
+        switch (op) {
+            case '+':
+                return bigDecimal1.add(bigDecimal2).floatValue();
+            case '-':
+                return bigDecimal1.subtract(bigDecimal2);
+            case '*':
+                return bigDecimal1.multiply(bigDecimal2);
+            case '/':
+                return bigDecimal1.divide(bigDecimal2);
+            default:
+                throw new IllegalArgumentException("Error! Enter correct operator " + op);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return num1 + " " + op + " " + num2;
+    }
+}
