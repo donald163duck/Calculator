a2d63bf960fa25232c6ab0bfadd3eabb6bdaf5b8
donald163duck <alexpodrolla@gmail.com>
2022-12-18 13:56:10 +0400

    Добавление теста для SimpleOperation

--- src/main/java/SimpleOperationTest.java
+++ src/main/java/SimpleOperationTest.java
@@ -1,4 +1,7 @@
 import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 public class SimpleOperationTest {
 
@@ -9,8 +12,11 @@ public class SimpleOperationTest {
             simpleOperation.setNum1(2);
             simpleOperation.setNum2(3);
             simpleOperation.setOp('+');
+
             Number result = simpleOperation.getResult();
-            assertEquals(5.0, result);
+            Float rt = 5.0F;
+            assertEquals(rt, result);
+            System.out.println("Test OK!");
     }
 
 
